a724dc14fa6525cda7e41183f11f53e79db86ae7
Renoy <renoy.chowdhury@hrm.group>
2025-01-20 17:02:36 +0100

    Initial Commit

---
+++ DesignPatternsStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignPatternsStudy
+//
+//  Created by Renoy on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignPatternsStudy/DesignPatternsStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsStudyApp.swift
+//  DesignPatternsStudy
+//
+//  Created by Renoy on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
